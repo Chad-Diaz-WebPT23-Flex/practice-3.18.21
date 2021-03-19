0b75e93f257596ddeda6f8413b02e2aacbd8f5f3
ChadDiaz <chadcdiaz@gmail.com>
2021-03-18 20:41:05 -0400

    why?

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,1 +1,12 @@
 // Write your "actions" router here!
+const router = require('express').Router();
+const Actions = require("./actions-model.js");
+
+router.get("/", async (req, res, next) => {
+  try {
+    const action = await Actions.get();
+    res.status(200).json(action);
+  } catch (err) {
+    next({ error: err, message: `error retrieving user`, status: 500 });
+  }
+});
--- api/server.js
+++ api/server.js
@@ -1,7 +1,25 @@
-const express = require('express');
+const express = require("express");
+
+const actionsRouter = require("./actions/actions-router.js");
+const projectsRouter = require("./projects/projects-router.js");
+const helmet = require("helmet");
 const server = express();
+
+server.use(helmet(), express.json());
+
+server.use("/api/actions", actionsRouter);
+server.use("/api/projects", projectsRouter);
+
+server.get("/", (req, res) => {
+  res.send(`<h2>Server Up</h2>`);
+});
 
 // Complete your server here!
 // Do NOT `server.listen()` inside this file!
+
+server.use((error, req, res, next) => {
+  error.error && console.error(error.error);
+  res.status(error.status).json({ message: error.message });
+});
 
 module.exports = server;
